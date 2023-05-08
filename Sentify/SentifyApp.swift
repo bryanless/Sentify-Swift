a4d726eb99b26e55cda918db6e164e70232edd43
Bryan <71095749+bryanless@users.noreply.github.com>
2023-05-08 09:04:23 +0700

    Initial Commit

---
+++ Sentify/SentifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentifyApp.swift
+//  Sentify
+//
+//  Created by Bryan on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SentifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
