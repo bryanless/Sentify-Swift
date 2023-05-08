d578ad726fc8147fd5ea432dae252e6327f4f3fa
Bryan <71095749+bryanless@users.noreply.github.com>
2023-05-08 09:58:14 +0700

    Add fonts & keys-example

--- Sentify/SentifyApp.swift
+++ Sentify/SentifyApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SentifyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
