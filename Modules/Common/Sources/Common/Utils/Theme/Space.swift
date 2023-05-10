073ed87dc643fee175a7ac0db87ad5cff54a12a9
Bryan <71095749+bryanless@users.noreply.github.com>
2023-05-10 14:19:43 +0700

    Make some struct public

--- Modules/Common/Sources/Common/Utils/Theme/Space.swift
+++ Modules/Common/Sources/Common/Utils/Theme/Space.swift
@@ -7,15 +7,15 @@
 
 public struct Space {
   /// 0.0
-  static let none = 0.0
+  public static let none = 0.0
   /// 4.0
-  static let extraSmall = 4.0
+  public static let extraSmall = 4.0
   /// 8.0
-  static let small = 8.0
+  public static let small = 8.0
   /// 12.0
-  static let medium = 12.0
+  public static let medium = 12.0
   /// 16.0
-  static let large = 16.0
+  public static let large = 16.0
   /// 24.0
-  static let extraLarge = 24.0
+  public static let extraLarge = 24.0
 }
